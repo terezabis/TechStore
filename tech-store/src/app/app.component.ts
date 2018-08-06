4e331f4231cca021a4e610a069fc59e796cc47d3
Tereza Biserina <tbiserina@gmail.com>
2018-08-06 13:24:41 +0300

    initial architecture and Firebase settings are done

--- tech-store/src/app/app.component.ts
+++ tech-store/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBLmEjNNlSANh0UjpJfu16gol4nfuBf9gw',
+      authDomain: "techstore-e9877.firebaseapp.com",
+      databaseURL: "https://techstore-e9877.firebaseio.com",
+      projectId: "techstore-e9877",
+      storageBucket: "techstore-e9877.appspot.com",
+      messagingSenderId: "1035920129464"
+    })
+  }
+
+
 }
